f0a47fa55eba1341c025f9fd6a631a2b59359b8d
Cris Franco <francochavezcristian@gmail.com>
2023-12-20 13:14:02 -0600

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { counterModule } from './counter/app.counter.module';
+import { HeroesModule } from './Heroes/heroes.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule
+    ,counterModule
+    ,HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/counter/components/counter/app.counter.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template :`<h1>Hola counter</h1>
+            <h3>Counter: {{counter}}</h3>
+            <button (click)="increaseBy(+1)">+1</button>
+            <button (click)="increaseBy(-1)">-1</button>
+            <br>
+            <button (click)="resetCounter()">Reset Counter</button>
+  `
+})
+
+export class CounterComponent{
+
+  public counter : number = 10;
+
+   increaseBy(value:number):void{
+        this.counter+=value;
+  }
+
+  resetCounter ():void{
+    this.counter = 10;
+  }
+
+}
